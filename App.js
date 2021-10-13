9b2a8c7e6bedd37bbe5c48c4f930d8cd078371a8
Rachel ShenHom <nshrachel@Rachels-Air.attlocal.net>
2021-10-13 12:34:18 -0400

    adding login and signUp page

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 import firebase from 'firebase/app';
 
 
@@ -27,16 +28,69 @@ if (firebase.apps.length === 0) {
 
 //using react navigation
 const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initalRouteName="Landing">
-        <Stack.Screen name = "Landing" component={LandingScreen} options={{headerShown: false}}/>
-        <Stack.Screen name = "Register" component={RegisterScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
 
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      loaded: false,
+    }
+  }
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+
+        })
+
+      }else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const {loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <Text >Loading</Text>
+        </View>
+      )
+
+    }
+    if(!loggedIn){
+      return (
+      
+        <NavigationContainer>
+          <Stack.Navigator initalRouteName="Landing">
+            <Stack.Screen name = "Landing" component={LandingScreen} options={{headerShown: false}}/>
+            <Stack.Screen name = "Register" component={RegisterScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+    
+      );
+
+    }
+    return(
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text >User is logged in</Text>
+      </View>
+    )
+    
+      
+        
+    
+    
+  }
 }
+
+export default App
+
+
 
 
